3d78757143eaf391c21e25718d6c9ed06756326e
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-23 06:43:02 +0600

    - fixed minor auth provider bug

--- src/Components/Authentication/AuthProvider.jsx
+++ src/Components/Authentication/AuthProvider.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext.jsx ';
 
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
 
-    const login = (userData)=>{
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+
+    const login = (userData) => {
         setUser(userData)
-        localStorage.setItem(user, userData)
+        localStorage.setItem("user", JSON.stringify(userData))
     }
 
-    const logOut = ()=>{
+    const logOut = () => {
         setUser(null)
         localStorage.removeItem('user')
     }
-    
+
     return (
         <AuthContext.Provider value={{ user, login, logOut }}>
             {children}
