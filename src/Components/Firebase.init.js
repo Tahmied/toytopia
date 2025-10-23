1b9950a13dc375c5109cb792e9ae002464a371f7
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-24 05:46:44 +0600

    - added environment variables

--- src/Components/Firebase.init.js
+++ src/Components/Firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPkkEt0emTgMxWgrLkoosA2vF-RmKu24E",
-  authDomain: "toytopia-8816f.firebaseapp.com",
-  projectId: "toytopia-8816f",
-  storageBucket: "toytopia-8816f.firebasestorage.app",
-  messagingSenderId: "423742485895",
-  appId: "1:423742485895:web:8db4d84e59c3e0069f0f22"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
